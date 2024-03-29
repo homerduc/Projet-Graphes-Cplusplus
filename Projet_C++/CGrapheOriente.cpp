9f7a2bc68d62ed9cdf435ad09843b8257123e6ad
Alexandre Duc <alexandre.duc@etu.univ-tours.fr>
2024-03-29 10:18:18 +0100

    constructeur de recopie CGrapheOrient

--- Projet_C++/CGrapheOriente.cpp
+++ Projet_C++/CGrapheOriente.cpp
@@ -1,5 +1,32 @@
 #include "CGrapheOriente.h"
 
+
+CGrapheOriente::CGrapheOriente(const CGrapheOriente& GROgraphe)
+{
+	vGROarcs.reserve(GROgraphe.GRO_GetArcs().size()); // On alloue d'avance la taille voulue
+	for (CArc* iIterateurArc : GROgraphe.GRO_GetArcs()) {
+		vGROarcs.push_back(new CArc(*iIterateurArc)); // On ajoute une copie de chaque arc dans la liste de notre nouveau sommet
+	}
+
+	vGROsommets.reserve(GROgraphe.GRO_GetSommets().size());
+	for (CSommet* iIterateurSommet : GROgraphe.GRO_GetSommets()) {
+		vGROsommets.push_back(new CSommet(*iIterateurSommet));
+	}
+}
+
+CGrapheOriente::~CGrapheOriente()
+{
+	for (CArc* iIterateurArc : vGROarcs) {
+		delete iIterateurArc;
+	}
+	vGROarcs.clear();
+
+	for (CSommet* iIterateurSommet : vGROsommets) {
+		delete iIterateurSommet;
+	}
+	vGROsommets.clear();
+}
+
 unsigned int CGrapheOriente::GRO_RechercheArcs(string sDepart, string sArrive)
 {
 	for (unsigned int uiPosition = 0; uiPosition < vGROarcs.size(); uiPosition++)
@@ -47,7 +74,6 @@ void CGrapheOriente::GRO_SupprimerSommet(string sRecherche)
 		vGROsommets.erase(vGROsommets.begin() + uiPosition);
 		cout << "Le sommet <" << sRecherche << "> a ete supprimer ! " << endl << endl;
 	}
-	
 }
 
 void CGrapheOriente::GRO_SupprimerArc(string sDepart, string sArrive)
